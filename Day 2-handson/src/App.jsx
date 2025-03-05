4b9095e470012950017b9257b0eed311a5293d95
Yuvikabhargava <86181668+Yuvikabhargava@users.noreply.github.com>
2025-03-05 10:23:11 +0530

    Add files via upload

---
+++ Day 2-handson/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import AddNum from './assets/AddNum'
+import Register from './assets/Register'
+
+function App() {
+  
+
+  return (
+    <>
+      <AddNum/>
+      <Register/>
+    </>
+  )
+}
+
+export default App
