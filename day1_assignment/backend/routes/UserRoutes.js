03758da9f189cd4a5bcef4ffa3cabf3113c00c54
Yuvikabhargava <86181668+Yuvikabhargava@users.noreply.github.com>
2025-03-10 12:36:15 +0530

    Delete day1_assignment/backend directory

--- day1_assignment/backend/routes/UserRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getUsers, addUser } = require("../controllers/userController");
-
-const router = express.Router();
-
-router.get("/", getUsers);
-router.post("/", addUser);
-
-module.exports = router;
