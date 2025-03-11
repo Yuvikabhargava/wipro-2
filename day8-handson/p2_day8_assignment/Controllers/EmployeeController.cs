a7a519fc657374f0045ed86285614bbf7f3ac92d
Yuvikabhargava <86181668+Yuvikabhargava@users.noreply.github.com>
2025-03-11 19:01:09 +0530

    Add files via upload

---
+++ day8-handson/p2_day8_assignment/Controllers/EmployeeController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using p2_day8_assignment.Models;
+using System.Linq;
+
+namespace p2_day8_assignment.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        public IActionResult Index()
+        {
+            var employees = _context.Employees.ToList();
+            return View(employees);
+        }
+
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Add(employee);
+                _context.SaveChanges();
+                return RedirectToAction("Index");  /
+            }
+            return View(employee);
+        }
+
+        
+        public IActionResult Edit(int id)
+        {
+            var employee = _context.Employees.FirstOrDefault(x => x.ID == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        
+        [HttpPost]
+        public IActionResult Edit(int id, Employee employee)
+        {
+            var existingEmployee = _context.Employees.FirstOrDefault(x => x.ID == id);
+            if (existingEmployee == null)
+            {
+                return NotFound();
+            }
+
+            existingEmployee.Name = employee.Name;
+            existingEmployee.Salary = employee.Salary;
+            existingEmployee.DateOfBirth = employee.DateOfBirth;
+            existingEmployee.DateOfJoining = employee.DateOfJoining;
+            
+
+            _context.SaveChanges();
+            return RedirectToAction("Index"); 
+        }
+
+        
+        public IActionResult Delete(int id)
+        {
+            var employee = _context.Employees.FirstOrDefault(x => x.ID == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var employee = _context.Employees.FirstOrDefault(x => x.ID == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Employees.Remove(employee);
+            _context.SaveChanges();
+            return RedirectToAction("Index"); 
+        }
+    }
+}
+
+
+
---
+++ day8-handson/p2_day8_assignment/Migrations/20250310172002_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace p2_day8_assignment.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateOfJoining = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Salary = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    Dept = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
